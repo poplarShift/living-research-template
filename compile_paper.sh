3502cc3355b7e2ebe89bd891ca3ec379270f7352
Achim Randelhoff <achim.randelhoff@takuvik.ulaval.ca>
2021-06-09 19:05:22 -0400

    revert the 'leanification'

--- compile_paper.sh
+++ compile_paper.sh
@@ -18,20 +18,28 @@ fi
 
 echo Using bibfile: $bib
 
-# --- compile  draft
+# include height of each HTML element in markdown
+# should find a better solution than Selenium+PhantomJS (deprecation warning)
+python add_html_heights_to_interactive_markdown.py ${file}_interactive.md
+
+# --- compile draft
 # -docx
 pandoc --bibliography $bib --filter pandoc-crossref --filter pandoc-citeproc --csl $cslfile --reference-doc reference.docx --mathjax -o ${file}.docx ${file}_static.md
 
 # now that draft is done, move figures up into text
 python move_figures_into_text.py ${file}_static.md
+python move_figures_into_text.py ${file}_interactive.md
 # --- static paper
 # -html
 pandoc --bibliography $bib --filter pandoc-crossref --filter pandoc-citeproc --csl $cslfile --self-contained --resource-path=.:../nb_fig/:../fig --mathjax -o ${file}_static.html ${file}_static.md
+
+# --- compile interactive paper
+pandoc --bibliography $bib --filter pandoc-crossref --filter pandoc-citeproc --csl $cslfile  --self-contained --resource-path=.:../nb_fig/:../fig --mathjaxn -o ${file}_interactive.html ${file}_interactive.md
 
 # make version with tracked changes
 ./diff.sh
 
 # clean up
-rm ${file}_static.md
+rm ${file}_static.md ${file}_interactive.md
 
 cd .. # ./
